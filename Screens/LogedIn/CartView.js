dc8156492fc7ac9760f0002a8bfe8c5b6646aa7d
maiphammm <ngocmaipham196@gmail.com>
2024-04-17 01:02:01 +1000

    cart

--- Screens/LogedIn/CartView.js
+++ Screens/LogedIn/CartView.js
@@ -2,28 +2,34 @@ import React from 'react';
 import { StyleSheet, Text, View, FlatList, TouchableOpacity, Image } from 'react-native';
 
 const CartView = ({ route, navigation }) => {
-  const { cartItems } = route.params;
-  const { totalPrice } = route.params;
+  const cartItems = route?.params?.cartItems || [];
+  const totalItems = route?.params?.totalItems || 0;
+  const totalPrice = route?.params?.totalPrice || 0;
   
   return (
     <View style={styles.container}>
+      <Text style={styles.Heading}>Cart</Text>
+      {cartItems && cartItems.length > 0 ? ( 
       <FlatList
         data={cartItems}
         keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item }) => 
-        (
+        //keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
           <View style={styles.itemContainer}>
             <Text style={styles.itemName}>{item.name}</Text>
-            <Text style={styles.itemPrice}>${item.totalItemPrice.toFixed(2)}</Text>
+            <Text style={styles.itemPrice}>{item.price} AUD</Text>
           </View>
-        )
-      }
-      />
-
-      <Text style={styles.totalPrice}>Total Price: {totalPrice ? totalPrice.toFixed(2) : '0.00 AUD'}</Text>
-      <TouchableOpacity style={styles.checkoutButton} onPress={() => navigation.navigate('Checkout')}>
-        <Text style={styles.checkoutButtonText}>Checkout</Text>
-      </TouchableOpacity>
+        )}
+      /> ) : (
+        <Text style={styles.noItemText}>No items in the cart</Text>
+      )}
+      <View style={styles.footer}>
+        <Text style={styles.totalItem}>Total Items: {totalItems}</Text>
+        <Text style={styles.totalPrice}>Total Price: AUD {totalPrice.toFixed(2)}</Text>
+        <TouchableOpacity style={styles.checkoutButton} onPress={() => navigation.navigate('Checkout')}>
+          <Text style={styles.checkoutButtonText}>Checkout</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -33,20 +39,38 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
   },
+  Heading: {
+    fontSize: 25,
+    marginTop: 30,
+    marginBottom: 20,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  footer: {
+    borderTopWidth: 1,
+    borderTopColor: '#ccc',
+    paddingTop: 10,
+    alignItems: 'center',
+  },
   itemContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginBottom: 10,
   },
   itemName: {
-    fontSize: 18,
+    fontSize: 15,
   },
   itemPrice: {
-    fontSize: 18,
+    fontSize: 15,
     fontWeight: 'bold',
   },
+  totalItem:{
+    fontSize: 15,
+    marginTop: 20,
+    alignSelf: 'flex-end',
+  },
   totalPrice: {
-    fontSize: 20,
+    fontSize: 17,
     fontWeight: 'bold',
     marginTop: 20,
     alignSelf: 'flex-end',
@@ -59,10 +83,16 @@ const styles = StyleSheet.create({
     marginTop: 20,
   },
   checkoutButtonText: {
-    fontSize: 18,
+    fontSize: 17,
     fontWeight: 'bold',
     color: '#fff',
     alignSelf: 'center',
+  },
+  noItemText: {
+    fontSize: 15,
+    textAlign: 'center',
+    marginTop: 40,
+    marginBottom: 40,
   },
 });
 
--- Screens/LogedIn/ProductView.js
+++ Screens/LogedIn/ProductView.js
@@ -1,31 +1,29 @@
+// ProductView.js
+
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, TextInput, ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 
 const ProductView = ({ route, navigation }) => {
-  // Destructuring the wigDB and imagePath from route params
   const { wigDB, imagePath } = route.params;
 
-  // State for managing quantity and cart items
   const [quantity, setQuantity] = useState(1);
   const [cartItems, setCartItems] = useState([]);
-  const [totalItems, setTotalItems] = useState(0); 
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalItems, setTotalItems] = useState(0);
+
+  const handleQuantityChange = (text) => {
+    const newQuantity = parseInt(text) || 1;
+    setQuantity(newQuantity);
+  };
 
   const handleAddToCart = () => {
-    // Create a new item object with the properties from wigDB, along with quantity and totalPrice
     const newItem = { ...wigDB, quantity, totalItemPrice: wigDB.price * quantity };
-
-    // Update the totalItems count by adding the quantity of the new item
-    setTotalItems(totalItems + quantity); 
-
-    // Add the new item to the cartItems array
+    const newTotalPrice = totalPrice + newItem.totalItemPrice; // Calculate new total price
     setCartItems([...cartItems, newItem]);
-
-    // Reset the quantity back to 1
-    setQuantity(1); 
-
-    // Navigate to the 'Cart' screen and pass the cartItems as a parameter
-    navigation.navigate('Cart', { cartItems: cartItems });
+    setTotalPrice(newTotalPrice); // Update total price
+    setTotalItems(totalItems + quantity);
+    navigation.navigate('Cart', { cartItems: [...cartItems, newItem], totalItems: totalItems + quantity, totalPrice: newTotalPrice });
   };
 
   return (
@@ -34,28 +32,27 @@ const ProductView = ({ route, navigation }) => {
         <Icon onPress={()=>{navigation.goBack()}} style={styles.Icons} name="chevron-left" />
       </View>
       <View style={styles.BottomView}>
-        {/* <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButton}>
-            <Text style={styles.backButtonText}>Back</Text>
-        </TouchableOpacity> */}
         <Image source={imagePath ? imagePath : { uri: wigDB.image }} style={styles.image} resizeMode="cover" />
         <View style={styles.detailsContainer}>
           <Text style={styles.name}>{wigDB.name}</Text>
           <Text style={styles.price}>{wigDB.price}</Text>
           <TextInput
-          style={styles.quantityInput}
-          placeholder="Quantity"
-          keyboardType="numeric"
-          value={quantity.toString()}
-          onChangeText={(text) => setQuantity(parseInt(text) || 1)}
+            style={styles.quantityInput}
+            placeholder="Quantity"
+            keyboardType="numeric"
+            value={quantity.toString()}
+            onChangeText={handleQuantityChange}
           />
           <TouchableOpacity onPress={handleAddToCart} style={styles.addToCartButton}>
-          <Text style={styles.addToCartButtonText}>Add to Cart ({totalItems})</Text>
+            <Text style={styles.addToCartButtonText}>Add to Cart</Text>
           </TouchableOpacity>
-          <Text style={styles.text}>{wigDB.cap}</Text>
-          <Text style={styles.text}>{wigDB.length}</Text>
-          <Text style={styles.text}>{wigDB.weight}</Text>
-          <Text style={styles.text}>{wigDB.density}</Text>
-          <Text style={styles.text}>{wigDB.handmade}</Text>
+          <ScrollView style={styles.details}>
+            <Text style={styles.text}>{wigDB.cap}</Text>
+            <Text style={styles.text}>{wigDB.length}</Text>
+            <Text style={styles.text}>{wigDB.weight}</Text>
+            <Text style={styles.text}>{wigDB.density}</Text>
+            <Text style={styles.text}>{wigDB.handmade}</Text>
+          </ScrollView>
         </View>
       </View>
     </View>
@@ -73,17 +70,6 @@ const styles = StyleSheet.create({
     height: '75%',
     display: 'flex',
     alignItems: 'center',
-  },
-  backButton: {
-    position: 'absolute',
-    top: 20,
-    left: 20,
-    zIndex: 1,
-  },
-  backButtonText: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#AF005F',
   },
   image: {
     width: 200,
@@ -138,6 +124,9 @@ const styles = StyleSheet.create({
     marginLeft: 5,
     color: '#000',
     fontSize: 40,
+  },
+  details: {
+    marginTop: 5,
   },
 });
 
